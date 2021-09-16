90c44917717e5f109440b33e8fa8348c1e9b5844
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-09-16 15:47:24 +0530

    Add DocTypes
    Add DocType for Item, Party, Account, Fiscal Year, Items, Sales Invoice, Purchase Invoice, Accounting Entries and Journal_Entry.

---
+++ accounting/accounting/doctype/accounting_entries/accounting_entries.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Sagar Sharma and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Accounting Entries', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ accounting/accounting/doctype/accounting_entries/test_accounting_entries.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Sagar Sharma and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAccountingEntries(unittest.TestCase):
+	pass
