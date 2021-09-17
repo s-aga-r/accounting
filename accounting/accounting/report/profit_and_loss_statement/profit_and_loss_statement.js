c4b6fa089aa7bc1b045d875afa28f6931100fa59
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-09-17 17:35:28 +0530

    Add Reports and Web Pages
    Add Report for General Ledger Report, Trial Balance, Balance Sheet and Profit and Loss Statement.
    Add Home and About Us Web Page.
    Modify some Website Settings.

---
+++ accounting/accounting/report/profit_and_loss_statement/profit_and_loss_statement.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Sagar Sharma and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Profit and Loss Statement"] = {
+	"filters": [
+
+	]
+};
