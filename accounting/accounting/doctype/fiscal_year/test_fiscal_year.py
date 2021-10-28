7d144f5d927e726fa55954ffa6a7d3312e746979
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-10-28 17:37:31 +0530

    test: Add test for Fiscal Year

--- accounting/accounting/doctype/fiscal_year/test_fiscal_year.py
+++ accounting/accounting/doctype/fiscal_year/test_fiscal_year.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2021, Sagar Sharma and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 import unittest
+from datetime import date
+from accounting.accounting.doctype.fiscal_year.fiscal_year import FiscalYear
+
 
 class TestFiscalYear(unittest.TestCase):
-	pass
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        frappe.db.delete("Fiscal Year")
+        print("Fiscal Year -> Passed")
+
+    def test_create_fiscal_year(self):
+        current_year = date.today().year
+        next_year = current_year + 1
+        self.assertEqual(FiscalYear.get_current_fiscal_year(),
+                         f"{current_year}-{next_year}")
