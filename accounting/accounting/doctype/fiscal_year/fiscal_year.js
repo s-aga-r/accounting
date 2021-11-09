27c087fc97bca6f5c338a3415a3fd6084c2a7785
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-11-09 09:35:25 +0530

    feat: Add "Ledger" button in Fiscal Year

--- accounting/accounting/doctype/fiscal_year/fiscal_year.js
+++ accounting/accounting/doctype/fiscal_year/fiscal_year.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2021, Sagar Sharma and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Fiscal Year', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Fiscal Year", {
+	refresh: function (frm) {
+		frm.add_custom_button("Ledger", function () {
+			frappe.route_options = {
+				"from_date": frm.doc.start_date,
+				"to_date": frm.doc.end_date,
+			};
+			frappe.set_route("query-report", "General Ledger Report");
+		}, "fa fa-table");
+	}
 });
+
