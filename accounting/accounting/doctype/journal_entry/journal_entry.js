a37ab9a8d15d4fc7d80a4098ae4fbd794b350694
Sagar Sharma <sagarsharma.s312@gmail.com>
2021-11-09 10:00:01 +0530

    feat: Add "Ledger" button in Journal Entry

--- accounting/accounting/doctype/journal_entry/journal_entry.js
+++ accounting/accounting/doctype/journal_entry/journal_entry.js
@@ -2,9 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Journal Entry', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		if (frm.doc.docstatus > 0) {
+			frm.add_custom_button('Ledger', function () {
+				frappe.route_options = {
+					"voucher_no": frm.doc.name,
+					"from_date": "",
+					"to_date": ""
+				};
+				frappe.set_route("query-report", "General Ledger Report");
+			}, "fa fa-table");
+		}
+	}
 });
 
 frappe.ui.form.on('Accounting Entries', {
